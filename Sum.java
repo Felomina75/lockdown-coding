abff9a43ab26527eb7094231e2917c0caad74e1e
Felomina75 <53473016+Felomina75@users.noreply.github.com>
2020-06-04 23:12:15 +0530

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Sum {
+
+  public static void main(String[] args){       
+    int i,n=0,sum=0;
+    System.out.println("Enter the 3 numbers : ");  
+		for (i=0;i<3;i++)
+		{
+		    Scanner in = new Scanner(System.in);
+		    n = in.nextInt();
+		    sum +=n;
+		}
+	System.out.println("The sum of 3 numbers is : " +sum+"\n);
+ 
+}
+}
